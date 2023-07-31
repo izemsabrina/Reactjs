0b07f188c0066780dcff42ccf039a6890a38fe3e
sabrina <sabrinizem@gmail.com>
2023-07-31 22:57:12 +0200

    ajouter une condition sur le footer

--- src/composants/Footer.js
+++ src/composants/Footer.js
@@ -1,7 +1,11 @@
-function Footer (){
+function Footer (props){
   
     return (
-        <footer><p>The weight is Lower than one tonne!!</p></footer>
+        <footer><p>The weight is 
+            {props.weight>1000 
+            ? <span className="red">Higher</span>
+            :<span className="green">Lower</span>}
+             than one tonne!!</p></footer>
     )
 }
 export default Footer;
